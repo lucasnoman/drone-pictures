f6c50b27fae52044ee84f4c81e262bf6a8ed2388
Lucas C. Noman <lucas.noman@outlook.com>
2022-12-27 22:41:27 -0300

    chore: configura tailwind e baixa icons phospor

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif',
+      },
+      backgroundImage: {
+        hero: 'url(../../public/images/home/hero.jpg)',
+        civil_construction: 'linear-gradient(180deg, rgba(0, 0, 0, 0) 40%, rgba(40,40,40, 0.8) 85%), url(../../public/images/home/civil_construction.jpg)',
+        real_state_market:'linear-gradient(180deg, rgba(0, 0, 0, 0) 40%, rgba(40,40,40, 0.8) 85%), url(../../public/images/home/real_state_market.jpg)',
+        content_1: 'url(../../public/images/home/content_1.jpg)',
+        content_2: 'url(../../public/images/home/content_2.jpg)',
+        virtual_tour: 'linear-gradient(0deg, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), url(../../public/images/home/virtual_tour.jpg)',
+      },
+      colors: {
+        dp_green: {
+          800: '#203A43',
+          400: '#53808F',
+        },
+        dp_orange: {
+          900: '#EC4F0B',
+        },
+        dp_light: {
+          100: '#FFFDFA',
+          200: '#F2F7F8',
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar-hide')],
 };
