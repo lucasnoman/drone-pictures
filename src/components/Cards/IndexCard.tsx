9fdb21c2b2bd0fc66db424fa8dd5d74f41fe7829
Lucas C. Noman <lucas.noman@outlook.com>
2023-07-02 22:28:26 -0300

    refactor: changes index card css organization with tailwind-variants

--- src/components/Cards/IndexCard.tsx
+++ src/components/Cards/IndexCard.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { tv } from 'tailwind-variants';
 
 interface IndexCardProps {
   title: string;
@@ -6,6 +7,18 @@ interface IndexCardProps {
   link: string;
   bgImage: string;
 }
+
+const card = tv({
+  slots: {
+    wrapper:
+      'h-full w-full lg:relative lg:bottom-12 lg:h-64 lg:w-80 lg:rounded-sm lg:outline lg:outline-1 lg:outline-offset-8',
+    image:
+      'h-full w-full flex items-end justify-center bg-cover bg-center duration-100 ease-in lg:rounded-md lg:hover:scale-105',
+    titleClass: 'pb-3 text-center text-dp_light-100 lg:font-bold',
+  },
+});
+
+const { wrapper, image, titleClass } = card();
 
 export function IndexCard({
   bgImage,
@@ -15,16 +28,10 @@ export function IndexCard({
 }: IndexCardProps) {
   return (
     <>
-      <div
-        className={`h-full w-full lg:relative lg:bottom-12 lg:h-64 lg:w-80 lg:rounded-sm lg:outline lg:outline-1 lg:outline-offset-8 lg:${borderColor}`}
-      >
+      <div className={`${wrapper()} ${borderColor}`}>
         <Link href={`${link}`} className='h-full w-full'>
-          <div
-            className={`flex h-full w-full items-end justify-center ${bgImage} bg-cover bg-center duration-100 ease-in lg:rounded-md lg:hover:scale-105`}
-          >
-            <p className='pb-3 text-center text-dp_light-100 lg:font-bold'>
-              {title}
-            </p>
+          <div className={`${image()} ${bgImage}`}>
+            <p className={titleClass()}>{title}</p>
           </div>
         </Link>
       </div>
